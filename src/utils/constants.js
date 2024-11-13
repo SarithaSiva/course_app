7fca0b2cf1f777ec04da9419bed52df4bbf7d5ca
SARITHA GANNAPANENI <gsarithacs07@gmail.com>
2024-11-13 14:45:10 -0800

    added all files

---
+++ src/utils/constants.js
@@ -0,0 +1,36 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'fevicon.ico';
+
+export const categories = [
+  { name: 'Automation Tools', icon: <SchoolIcon />, },
+  { name: 'Devops', icon: <SchoolIcon />, },
+  { name: 'Basic C', icon: <SchoolIcon />, },
+  { name: 'ReactJS', icon: <SchoolIcon />, },
+  { name: 'DataScience', icon: <SchoolIcon />, },
+  { name: 'Python', icon: <SchoolIcon /> },
+  { name: 'Java', icon: <SchoolIcon />, },
+  { name: 'FullStackDeveloper', icon: <SchoolIcon />, },
+  { name: 'Networking', icon: <SchoolIcon />, },
+  { name: 'Coding for kids', icon: <SchoolIcon />, },
+  { name: 'Cloud Computing', icon: <SchoolIcon />, }
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
